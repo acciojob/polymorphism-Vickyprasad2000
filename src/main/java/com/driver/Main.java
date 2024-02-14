f713c9a0098ccb2760ca65c7af2c79c254371855
Vickyprasad2000 <vickyprasadkn@gmail.com>
2024-02-14 14:38:09 +0530

    solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String args[])
+    {
+        Product p = new Product();
+        p.product(10,20);
+        p.product(100,200,300);
+        p.product(2.50,5.50);
+    }
+    static class Product{
+        public int product(int x, int y)
+        {
+            return x*y;
+        }
+        public int product(int x, int y,int z)
+        {
+            return x*y*z;
+        }
+        public double product(double x, double y)
+        {
+            return x*y;
+        }
+    }
 }
